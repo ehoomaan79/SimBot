3f09f86174352b46bc7108567dc5325fa9d024ac
Hoomaan Ebrahimi <ehoomaan@gmail.com>
2026-08-04 14:19:44 +0000

    updated discord commands

--- bot/bot.py
+++ bot/bot.py
@@ -21,7 +21,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 
 @bot.command(name="help")
